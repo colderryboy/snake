6be1717152025189860bb3dea865ae65d3b276cb
Cold <72723052+Colderry@users.noreply.github.com>
2021-09-10 21:19:59 +0600

    Create input.py

---
+++ input.py
@@ -0,0 +1,7 @@
+first = input("Write your first number")
+second = input("Write your second number")
+
+if first > second:
+    print("First number is greater than second number")
+elif first < second:
+    print("First number is less than second number")
