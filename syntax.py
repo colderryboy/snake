72abfccebb0244b03bca0ed40c7b2260bca73865
Cold <72723052+Colderry@users.noreply.github.com>
2021-09-10 21:53:55 +0600

    Create syntax.py

---
+++ syntax.py
@@ -0,0 +1,15 @@
+if 69 > 0: 
+    print("Yes") # Indentation required
+
+if 6969 > 0:
+                                        
+
+        # You can give as much as space you want LOL                
+                                
+                                                
+                                                print("xD")
+
+# Variables
+x = "I am"
+n = " Colderry"
+print(x + n)
