22c26042f304cc21715fcd72d96226a4d2a2827e
Cold <72723052+Colderry@users.noreply.github.com>
2021-09-13 18:06:14 +0600

    Create casting.py

---
+++ casting.py
@@ -0,0 +1,11 @@
+# Integer
+print(int(10.69)) # Result 10
+print(int("5")) # Result 5
+
+# Floats
+print(float(1)) # Result 1
+print(float("6")) # Result 6.0
+
+# String
+print(str(2)) # Result '2'
+print(str(6.9)) # Result '6.9'
